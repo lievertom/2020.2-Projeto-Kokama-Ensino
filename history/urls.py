2d2b4b0e9ab76caa16df02755d5cc9eac90d659b
Fernando <nandovargas7@gmail.com>
2021-04-06 19:47:23 -0300

    create function to delete and edit
    Co-authored-by: Lucas Monteiro <machiromonteiro@gmail.com>

--- history/urls.py
+++ history/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views_html.list_history, name='list_history'),
     path('adicionar-historias/', views_html.add_history, name='add_history'),
     path('<int:id>/', views_html.views_history, name='views_history'),
+    path('<int:id>/delete', views_html.del_history, name='del_history'),
+    path('<int:id>/edit', views_html.edit_history, name='edit_history'),
 ]
--- history/views_html.py
+++ history/views_html.py
@@ -43,3 +43,29 @@ def views_history(request, id):
         return render(request, 'views_history.html', context)
     else:
         return HttpResponse('<h1>Erro interno do servidor</h1>', status=500)
+
+@require_http_methods(["GET"])
+def del_history(request, id):
+    if(request.method == 'GET'):
+        emp = KokamaHistory.objects.get(pk = id)
+        emp.delete()
+        return redirect('/')
+
+    return HttpResponse('Erro ao deletar', status=500)
+
+@require_http_methods(["GET", "POST"])
+def edit_history(request, id):
+    if(request.method == 'GET'):
+        form = AddNewHistory()
+        return render(request, 'add_history.html', {'form': form})
+    elif(request.method == 'POST'):
+        form = AddNewHistory(request.POST)
+        
+        if form.is_valid():
+            history = KokamaHistory.objects.get(pk = id)
+            history.history_title = request.POST.get('history_title')
+            history.history_text = request.POST.get('history_text')
+            history.save()
+            return redirect('/')
+
+    return redirect('/')
