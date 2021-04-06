586346b4fda98f19525414db05948d18a12bc0ae
Fernando <nandovargas7@gmail.com>
2021-04-06 19:37:18 -0300

    Create function and page for the registration historys
    Co-authored-by: Lucas Monteiro <machiromonteiro@gmail.com>

--- history/urls.py
+++ history/urls.py
@@ -3,4 +3,5 @@ from . import views_html
 
 urlpatterns = [
     path('', views_html.list_history, name='list_history'),
+    path('adicionar-historias/', views_html.add_history, name='add_history'),
 ]
--- history/views_html.py
+++ history/views_html.py
@@ -12,3 +12,18 @@ def list_history (request):
         return render(request, 'list_history.html',{'object':history_title})
     else:
         return HttpResponse('<h1>Erro interno do servidor</h1>', status=500)
+
+@require_http_methods(["GET", "POST"])
+def add_history(request):
+    if(request.method == 'GET'):
+        form = AddNewHistory()
+        return render(request, 'add_history.html', {'form': form})
+    elif(request.method == 'POST'):
+        form = AddNewHistory(request.POST)
+        
+        if form.is_valid():
+            history_title = request.POST.get('history_title')
+            history_text = request.POST.get('history_text')
+            history = KokamaHistory(history_title=history_title, history_text=history_text)
+            history.save()
+            return redirect('/')
