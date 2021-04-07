cbe40ed9ed85ed3c05d634f15d568389e8398a50
luisgaboardi <luis.ggaboardi@gmail.com>
2021-04-06 21:45:34 -0300

    Get and visualize words and phrases from Translate Service
    Co-authored-by: Lucas Rodrigues <machiromonteiro@gmail.com>

--- exercise/views.py
+++ exercise/views.py
@@ -1,1 +1,12 @@
 from django.shortcuts import render
+import requests
+
+
+def home(request):
+    response_words = requests.get('https://run.mocky.io/v3/92f3a2e6-1686-454b-9847-5e625bc5959f')
+    words = response_words.json()
+
+    response_phrases = requests.get('https://run.mocky.io/v3/4d176167-1c9c-45af-a74e-679f949cbd0e')
+    phrases = response_phrases.json()
+
+    return render(request, 'home.html', {'words': words, 'phrases': phrases})
