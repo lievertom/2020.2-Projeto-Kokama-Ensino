d48d7bf781856e08c510b100ecc562c2ae3ea470
Leonardo Medeiros <leonardomedeiros.6@gmail.com>
2021-04-15 13:33:11 -0300

    Fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # entrypoint.sh
 
-python3 manage.py makemigrations 
+python3 manage.py makemigrations
+python3 manage.py makemigrations history exercise
 python3 manage.py migrate
 python3 manage.py migrate --run-syncdb
 python3 manage.py runserver 0.0.0.0:8001
