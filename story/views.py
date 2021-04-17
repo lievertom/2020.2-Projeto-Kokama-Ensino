07a72f174b816215d002471ca259a02b69e0d1ac
luisgaboardi <luis.ggaboardi@gmail.com>
2021-04-17 08:25:47 -0300

    Finish refactor in learn service. All functions and pages implemented
    Improve search in story search query

--- story/views.py
+++ story/views.py
@@ -18,13 +18,22 @@ class StoryViewSet(viewsets.ModelViewSet):
     queryset = Story.objects.all()
     serializer_class = StorySerializer
 
-@api_view(["POST"])
+    def destroy(self, request, *args, **kwargs):
+        try:
+            story = self.get_object()
+            story.delete()
+        except:
+            return Response(status=HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response(status=HTTP_204_NO_CONTENT)
+
+
+@api_view(["GET", "POST"])
 def add_story(request, id):
     story = Story.objects.none()
     # Edit
     if id:
         story = Story.objects.get(id=id) # Conferir se existe (try)
-        if story.title != request.POST.get('title') and Story.objects.filter(title=request.POST.get('title')).first():
+        if story.title != request.POST.get('title') or Story.objects.filter(title=request.POST.get('title')).first():
             return Response(
                 {'error': 'Narrativa já cadastrada.'},
                 status=HTTP_400_BAD_REQUEST,
@@ -33,9 +42,7 @@ def add_story(request, id):
         story.delete()
 
     story, created = Story.objects.get_or_create(
-        id=request.POST.get('id'),
         title=request.POST.get('title'),
-        text=request.POST.get('text'),
     )
     if not created:
         return Response(
