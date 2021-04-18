f2e721081ce3c08b58f32b55e7b693b7e5573f65
luisgaboardi <luis.ggaboardi@gmail.com>
2021-04-17 21:30:45 -0300

    Order story list by newer id
    Co-authored-by: Andre Lucas <andrelucax@gmail.com>

--- story/views.py
+++ story/views.py
@@ -3,5 +3,5 @@ from .models import Story
 from .serializers import StorySerializer
 
 class StoryViewSet(viewsets.ModelViewSet):
-    queryset = Story.objects.all()
+    queryset = Story.objects.all().order_by('-id')
     serializer_class = StorySerializer
