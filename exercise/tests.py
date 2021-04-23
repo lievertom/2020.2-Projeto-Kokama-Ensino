99e33d5996d85546e3363a32ca2840f7d4e0f966
MacLovin <monteiro9961@gmail.com>
2021-04-23 00:36:54 -0300

    Initial unit testing.
    Co-authored-by: Ana Julia <aluzianobriceno@gmail.com>

--- exercise/tests.py
+++ exercise/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from .models import Activity, Contain, Option
+from story.models import Story
 
-# Create your tests here.
+class testUtil(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        
+    def test_pra_dar_ruim(self):
+        request = self.client.get('/historia/lista_de_historias') 
+        self.assertEquals(200, request.status_code) 
+    
+    def test_bar(self):
+        data = {'text': '20', 'title':'30'}
+        request = self.client.post('/historia/lista_de_historias', data, format='json')
+        story = Story.objects.last()
+        self.assertEquals(data['text'], story.text)
+
+
