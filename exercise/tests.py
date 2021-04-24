5ffb43bc17c3f6d3ad4bf7eb0c5c972815b5cca1
MacLovin <monteiro9961@gmail.com>
2021-04-24 19:32:45 -0300

    microservice unit tests.
    Co-authored-by: Ana Julia <aluzianobriceno@gmail.com>

--- exercise/tests.py
+++ exercise/tests.py
@@ -1,20 +1,37 @@
 from django.test import TestCase
-from rest_framework.test import APIClient
-from .models import Activity, Contain, Option
-from story.models import Story
+from ..views import ActivityViewSet
 
-class testUtil(TestCase):
-    def setUp(self):
-        self.client = APIClient()
-        
-    def test_pra_dar_ruim(self):
-        request = self.client.get('/historia/lista_de_historias') 
-        self.assertEquals(200, request.status_code) 
+
+class ActivityViewSetTest(TestCase):
+    def setUp(self) -> None:
+        self.phrase = ActivityViewSet(field = 'phrase-kokama')
+        self.remover = ActivityViewSet(remove = 'remove')
+        self.serializer = ActivityViewSet(many=True, read_only=True, slug_field='option')
+
+    def test_str_get_data(self):
+        phrase = Peixe.objects.create(field = 'phrase-kokama')
+        expected = 'nós/ini'
+        result = str(self.phrase)
+        self.assertEqual(expected, result)
+
+    def test_str_clean_database(self):
+        phrase = Phrase.objects.create(field = 'phrase-kokama')        
+        remover = remover.objects.creature(remove = 'remove')
+        expected = 'kokama'
+        result = str(self.phrase.remover)
+        self.assertEqual(expected, result)
+
+    def test_add_possible_options(self):
+        phrase = Phrase.objects.create(field = 'phrase-kokama')        
+        serializer = serializer.objects.creature(many=True, read_only=True, slug_field='option')
+        expected = 'kokama'
+        result = str(self.phrase.serializer)
+        self.assertEqual(expected, result)
+
+    def test_not_possible_option(self):
+        phrase = phrase.objects.create('phrase-kokama')        
+        expected = False
+        result = str(self.phrase)
+        self.assertEqual(expected, result)
+
     
-    def test_bar(self):
-        data = {'text': '20', 'title':'30'}
-        request = self.client.post('/historia/lista_de_historias', data, format='json')
-        story = Story.objects.last()
-        self.assertEquals(data['text'], story.text)
-
-
